9bd3dd6af4a8df7c71ab9da30c4b4ff3f7073028
Sherdr <rainar_kuper@mail.ru>
2024-06-13 20:25:58 +0300

    Фильтрация сущностей.

--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -5,9 +5,11 @@ namespace WorkingWithEFCore {
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine($"Using {ProjectConstant.DatabaseProvider} provider.");
-            QueryingCategories();
+            FilteredIncludes();
         }
+
         static void QueryingCategories() {
+            Console.WriteLine("Categories and how many products they have:");
             using (Northwind db = new()) {
                 IQueryable<Category>? categories = db.Categories?
                     .Include(category => category.Products);
@@ -17,6 +19,26 @@ namespace WorkingWithEFCore {
                 }
                 foreach (Category c in categories) {
                     Console.WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+                }
+            }
+        }
+
+        static void FilteredIncludes() {
+            using (Northwind db = new()) {
+                Console.WriteLine("Enter a minimum for units in stock: ");
+                string unitsInStock = Console.ReadLine() ?? "10";
+                int stock = int.Parse(unitsInStock);
+                IQueryable<Category>? categories = db.Categories?
+                    .Include(category => category.Products.Where(product => product.Stock >= stock));
+                if (categories is null) {
+                    Console.WriteLine("No categories found.");
+                    return;
+                }
+                foreach (Category c in categories) {
+                    Console.WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock.");
+                    foreach (Product p in c.Products) {
+                        Console.WriteLine($"\t{p.ProductName} has {p.Stock} units in stock.");
+                    }
                 }
             }
         }
