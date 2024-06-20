b609cfe89e63a773192f2e4fae111b9fb5bd15b2
Sherdr <rainar_kuper@mail.ru>
2024-06-21 01:45:48 +0300

    Принцип отложенного запуска Linq.

---
+++ Chapter11/LinqWithObjects/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LinqWithObjects {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] names = new[] {
+                "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"
+            };
+            var query1 = names.Where(name => name.EndsWith("m"));
+            var query2 = from name in names where name.EndsWith("m") select name;
+
+            string[] result1 = query1.ToArray();
+            List<string> result2 = query2.ToList();
+
+            foreach (string name in result1) {
+                Console.WriteLine(name);
+                names[2] = "Jimmy";
+            }
+            Console.WriteLine("Deferred execution.");
+            foreach (string name in query1) {
+                Console.WriteLine(name);
+                names[2] = "Jimmy";
+            }
+        }
+    }
+}
