81f3bd0f52fd077ec94e7a8b121372ade562322d
Sherdr <rainar_kuper@mail.ru>
2024-06-26 00:38:33 +0300

    Использование LINQ с EFCore.

---
+++ Chapter11/LinqWithEFCore/Northwind.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared {
+    public class Northwind : DbContext {
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+
+        protected override void OnModelCreating(
+        ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Product>()
+            .Property(product => product.UnitPrice)
+            .HasConversion<double>();
+        }
+    }
+}
---
+++ Chapter11/LinqWithEFCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Packt.Shared;
+
+namespace LinqWithEFCore {
+    internal class Program {
+        static void Main(string[] args) {
+            FilterAndSort();
+        }
+        static void FilterAndSort() {
+            Console.WriteLine("Products that cost less than $10:");
+            using(Northwind db = new()) {
+                DbSet<Product> allProducts = db.Products;
+                IQueryable<Product> filteredProducts = allProducts.Where(prod => prod.UnitPrice < 10m);
+                IOrderedQueryable<Product> sortedProducts = filteredProducts.OrderByDescending(prod => prod.UnitPrice);
+                foreach (Product p in sortedProducts) {
+                    Console.WriteLine($"{p.ProductId}: {p.ProductName} costs {p.UnitPrice:$#,##0.00}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
