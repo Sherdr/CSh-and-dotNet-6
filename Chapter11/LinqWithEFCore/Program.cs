5fdd332457e7d75d69350cef2efbc5d184c06786
Sherdr <rainar_kuper@mail.ru>
2024-07-07 22:31:44 +0300

    Объединение последовательностей.

--- Chapter11/LinqWithEFCore/Program.cs
+++ Chapter11/LinqWithEFCore/Program.cs
@@ -4,8 +4,9 @@ using Packt.Shared;
 namespace LinqWithEFCore {
     internal class Program {
         static void Main(string[] args) {
-            FilterAndSort();
+            JoinCategoriesAndProducts();
         }
+
         static void FilterAndSort() {
             Console.WriteLine("Products that cost less than $10:");
             using(Northwind db = new()) {
@@ -22,6 +23,21 @@ namespace LinqWithEFCore {
                     Console.WriteLine($"{p.ProductId}: {p.ProductName} costs {p.UnitPrice:$#,##0.00}");
                 }
                 Console.WriteLine();
+            }
+        }
+
+        static void JoinCategoriesAndProducts() {
+            using (Northwind db = new()) {
+                var queryJoin = db.Categories.Join(
+                    db.Products,
+                    category => category.CategoryId,
+                    product => product.CategoryId,
+                    (c, p) => new { c.CategoryName, p.ProductName, p.ProductId }
+                ).
+                OrderBy(cp=>cp.CategoryName);
+                foreach (var item in queryJoin) {
+                    Console.WriteLine($"{item.ProductId}: {item.ProductName} is in {item.CategoryName}");
+                }
             }
         }
     }
